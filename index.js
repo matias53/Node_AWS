0bd40af27f2c5bc41de9deb1cd016be771cba207
Matias Settimo <mlsettimo@gmail.com>
2020-09-17 23:19:42 -0300

    2nd commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Koa = require("koa");
+const json = require("koa-json");
+const bodyParser = require("koa-bodyparser");
+const appPort = 80;
+
+const app = new Koa();
+
+app.use(json({ pretty: true }));
+app.use(bodyParser());
+app.listen(appPort, () =>
+    console.log(
+        "[App Initialization] App Running. Server Started on port " + appPort + "..."
+    )
+);
+
+
+app.use(ctx => {
+    ctx.body = 'Hello World';
+});
